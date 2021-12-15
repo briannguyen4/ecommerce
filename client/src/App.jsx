bba24eef517685ac0bd311e7072adb26991f5d47
briannguyen4 <brian4nguyen@gmail.com>
2021-12-15 01:45:22 -0500

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import {
   BrowserRouter as Router,
-  Switch,
-  Route
+  Routes,
+  Route,
+  Navigate
 } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
@@ -11,13 +12,33 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
 const App = () => {
+  const user = true;
   return (
     <Router>
-      <Switch>
-        <Route path="/">
-          <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />}>
+          <Route path=":category" element={<ProductList />} />
         </Route>
-      </Switch>
+        <Route path="/product">
+          <Route path=":id" element={<Product />} />
+        </Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={
+          user ? (
+            <Navigate to="/" />
+          ) : (
+            <Login/>
+          )}
+        />
+        <Route path="/register" element={
+          user ? (
+            <Navigate to="/" />
+          ) : (
+            <Register/>
+          )}
+        />
+      </Routes>
     </Router>
   );
 };
