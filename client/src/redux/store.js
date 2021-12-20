05dcf0c638e101504ed58ed951f7b47d920ef148
briannguyen4 <brian4nguyen@gmail.com>
2021-12-20 02:30:59 -0500

    create root reducer and add redux persist to it

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
 import {
@@ -18,14 +18,16 @@ const persistConfig = {
     version: 1,
     storage,
 };
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    cart: cartReducer
+});
   
-const persistedReducer = persistReducer(persistConfig, userReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        user: persistedReducer,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
